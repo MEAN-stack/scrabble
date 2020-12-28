be80c646db5a213db3b383733c2627f480f0c292
Paul Robertson <paul.robertson@costain.com>
2020-12-28 18:42:22 +0000

    Add routing, login, header components

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from './login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(
+    private loginService: LoginService,
+    private router: Router
+  ) { }
+
+  loginForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.loginForm.disable();
+    this.loginService.login(this.loginForm.value.username, this.loginForm.value.password).subscribe(
+      (response) => {
+        console.log("JWT: " + response.jwt);
+        this.router.navigate(['../game-page']);
+      },
+      (err) => {
+        console.log(err);
+        this.loginForm.enable();
+      },
+      () => {
+        this.loginForm.enable();
+      }
+    );
+  }
+}
---
+++ src/app/login/login.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+
+interface LoginResponse {
+  jwt: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  private heartbeatSubscription: Subscription;
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  login(username: string, password: string): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>("/api/sessions", { username: username });
+  };
+}
